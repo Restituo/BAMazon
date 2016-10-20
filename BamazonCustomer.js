e926836fb0d5c8f3c78235c7b4097a72d61d104b
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2016-10-20 14:43:41 -0400

    functioning app, need to change quantity after user purchase

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -15,3 +15,77 @@ var connection = mysql.createConnection({
 //   if(err) throw err;
 //   console.log("connected as id " + connection.threadId);
 // })
+
+function start(){
+//prints the items for sale and their details
+connection.query('SELECT * FROM Products', function(err, res){
+  if(err) throw err;
+
+  console.log('_.~"~._.~"~._.~Welcome to BAMazon~._.~"~._.~"~._')
+  console.log('----------------------------------------------------------------------------------------------------')
+
+  for(var i = 0; i<res.length;i++){
+    console.log("ID: " + res[i].ItemID + " | " + "Product: " + res[i].ProductName + " | " + "Department: " + res[i].DepartmentName + " | " + "Price: " + res[i].Price + " | " + "QTY: " + res[i].StockQuantity);
+    console.log('--------------------------------------------------------------------------------------------------')
+  }
+
+  console.log(' ');
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "id",
+      message: "What is the ID of the product you would like to purchase?",
+      validate: function(value){
+        if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
+          return true;
+        } else{
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "qty",
+      message: "How much would you like to purchase?",
+      validate: function(value){
+        if(isNaN(value)){
+          return false;
+        } else{
+          return true;
+        }
+      }
+    }
+    ]).then(function(ans){
+      var whatToBuy = (ans.id)-1;
+      var howMuchToBuy = parseInt(ans.qty);
+      var grandTotal = (res[whatToBuy].Price)*howMuchToBuy;
+
+      //check if quantity is sufficient
+      if(res[whatToBuy].StockQuantity >= howMuchToBuy){
+        console.log("Success! Your total is $" + grandTotal + ". Your item(s) will be shipped to you in 3-5 business days.");
+        reprompt();
+      } else{
+        console.log("Sorry, there's not enough in stock!");
+        reprompt();
+      }
+    });
+
+})
+}
+
+//asks if they would like to purchase another item
+function reprompt(){
+  inquirer.prompt([{
+    type: "confirm",
+    name: "reply",
+    message: "Would you like to purchase another item?"
+  }]).then(function(ans){
+    if(ans.reply){
+      start();
+    } else{
+      console.log("See you soon!");
+    }
+  });
+}
+
+start();
